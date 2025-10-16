ee7db82efdac25e0d4d845599e2f6ca7cb6a6d2d
RiKy-07 <ramosruikiamy2121@gmail.com>
2025-10-17 00:03:37 +0100

    Team section finished, next: Testimonials

---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+// let btn_sign = document.querySelectorAll('.sign_container');
+// let image =btn_sign[0].querySelector('img');
+
+// console.log(btn_sign)
+// //btn_sign.removeChild(image)
+// console.log(image.src);
+// image.src="/public/assets/svgs/minus.svg"
+
